a3ae76d77b0f3f26ddf133afa4aee56c9d643c70
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-24 21:16:25 -0500

    Ex: Answer to 3 steps equals answer to 2 steps + answer to 1 Step

--- JS/staircase.js
+++ JS/staircase.js
@@ -5,6 +5,11 @@ const stairCase = (steps, storage) => {
 	// Exit code for when stairs equal 0 or 1 step
 	if (steps <= 1) return 1;
 
+	// If we already found the result, return it
 	if (storage[steps]) return storage[steps];
+
+	// Ex: Answer to 3 steps equals answer to 2 steps + answer to 1 Step
+	let result = stairCase(steps - 1, storage) + stairCase(steps - 2, storage);
+	 
 
 }
