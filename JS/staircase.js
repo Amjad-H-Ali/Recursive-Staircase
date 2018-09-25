f370437162bb24e0c082bb0b682e2e3c3306e9c2
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-24 21:19:58 -0500

    Helper function

--- JS/staircase.js
+++ JS/staircase.js
@@ -1,7 +1,7 @@
 console.log("Staircase!");
 
 // Takes in amount of steps and array to store results to prevent redundant computation
-const stairCase = (steps, storage) => {
+const helper = (steps, storage) => {
 	// Exit code for when stairs equal 0 or 1 step
 	if (steps <= 1) return 1;
 
@@ -9,7 +9,7 @@ const stairCase = (steps, storage) => {
 	if (storage[steps]) return storage[steps];
 
 	// Ex: Answer to 3 steps equals answer to 2 steps + answer to 1 Step
-	let result = stairCase(steps - 1, storage) + stairCase(steps - 2, storage);
+	let result = helper(steps - 1, storage) + helper(steps - 2, storage);
 
 	// Store result in array before returning it
 	storage[steps] = result;
@@ -17,3 +17,8 @@ const stairCase = (steps, storage) => {
 	return result;
 
 }
+
+// Helper function
+const stairCase = steps => return helper(steps, []);
+
+
