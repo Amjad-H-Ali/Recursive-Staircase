8da48ae9e9f61cb2c028b473480c440f6ff50f81
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-24 21:06:47 -0500

    Exit code for when stairs equal 0 or 1 step

--- JS/staircase.js
+++ JS/staircase.js
@@ -2,6 +2,7 @@ console.log("Staircase!");
 
 // Takes in amount of steps and array to store results to prevent redundant computation
 const stairCase = (steps, storage) => {
-	
+	// Exit code for when stairs equal 0 or 1 step
+	if (steps <= 1) return 1;
 
 }
